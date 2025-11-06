d56220f58c86e8a51ec3d60b129bba3ad0432951
ian4734 <40573@ma-web.nl>
2025-11-06 12:15:36 +0100

    Add files via upload
    Opdracht 3.2

---
+++ MusicPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start();
+    {
+        void PlaySong("Minecraft OST - Sweden");
+        void StopSong("Minecraft OST - Sweden");
+        void setVolume("0,8");
+        string GetCurrentSong("true");
+        bool IsPlaying("False");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
