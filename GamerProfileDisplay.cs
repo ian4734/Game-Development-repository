8979b1ba2920531240511374961168108cf1e840
ian4734 <40573@ma-web.nl>
2025-11-06 11:56:43 +0100

    Add files via upload
    Script opdracht 1.2

---
+++ GamerProfileDisplay.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GamerProfileDisplay : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("start game");
+        // single line comment
+        // gaming profile Information
+       Debug.Log("===Gaming Profile");
+        Debug.Log("Gamer Tag: Ian");
+        Debug.Log("Favorite Game: WUWA");
+        Debug.Log("Gaming Platform: Mac");
+        Debug.Log("Hours Played Today: 2");
+        Debug.Log("Current Level: 42");
+        Debug.Log("Characters owned: 12");
+        Debug.Log("battery level: 98");
+        Debug.Log("hours played this week: 12");
+        Debug.Log("Achievement Unlocked: First Script!");
+        Debug.Log("==================");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
